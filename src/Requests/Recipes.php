1cbc1e74c7d83b2b3332270fcc2d12a09e057902
Bram Devries <bram@madewithlove.be>
2015-03-29 20:51:48 +0200

    Add searching for recipes

--- src/Requests/Recipes.php
+++ src/Requests/Recipes.php
@@ -6,6 +6,8 @@ use GuildWars2\Abstracts\AbstractRequest;
 
 class Recipes extends AbstractRequest
 {
+	const SEARCH_INPUT = 'input';
+	const SEARCH_OUTPUT = 'output';
 
 	/**
 	 * @param array $ids
@@ -19,4 +21,35 @@ class Recipes extends AbstractRequest
 		]);
 	}
 
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	public function searchOutput($id)
+	{
+		return $this->search($id, static::SEARCH_OUTPUT);
+	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	public function searchInput($id)
+	{
+		return $this->search($id, static::SEARCH_INPUT);
+	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	protected function search($id, $type)
+	{
+		return $this->get('recipes/search', [
+			$type => $id
+		]);
+	}
 }
